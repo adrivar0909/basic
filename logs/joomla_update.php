ca016abcb8d8839b053e4349f28b2c8d9fd8c3a1
adrian <adrivar0909@gmail.com>
2016-12-13 16:43:22 -0400

    Commit a la rama principal

---
+++ logs/joomla_update.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-12-16 12:39:48 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2014-12-16T12:39:48+00:00	INFO 188.165.133.115	update	COM_JOOMLAUPDATE_UPDATE_LOG_DELETE_FILES
